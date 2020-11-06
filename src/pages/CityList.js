aad6cba7ec97ef2ae52644a2e1b46e092b69aa69
AliFeyzi78 <zeron1g2k@gmail.com>
2020-11-06 22:53:53 +0200

    ana component lar olusturuldu

---
+++ src/pages/CityList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import { SafeAreaView, View, Text } from 'react-native-safe-area-context';
+
+const CityList = (props) => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>City List</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export { CityList };
